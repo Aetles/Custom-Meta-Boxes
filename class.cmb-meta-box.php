5d0738887b4c2d9aeecbaa57930b7688e63639e4
Adrian Bengtson <adrian@adagio.se>
2014-10-06 03:03:07 +0200

    Revert change that broke "show_on"
    This revert a change made in https://github.com/humanmade/Custom-Meta-Boxes/commit/e74409af67c6e00bba9161b4ea7efa1f1f0ba2c4 that broke the "show_on" as far as I can tell.
    
    Note: reverting these lines could cause other issues, this is not very well tested.

--- class.cmb-meta-box.php
+++ class.cmb-meta-box.php
@@ -119,9 +119,8 @@ class CMB_Meta_Box {
 			unset( $this->_meta_box['show_on']['value'] );
 		}
 
+		if ( $this->is_metabox_displayed( $this->_meta_box ) ) {
 		foreach ( (array) $this->_meta_box['pages'] as $page ) {
-			$show = apply_filters( 'cmb_show_on', true, $this->_meta_box );
-			if ( $show ) {
 				add_meta_box( $this->_meta_box['id'], $this->_meta_box['title'], array(&$this, 'show'), $page, $this->_meta_box['context'], $this->_meta_box['priority'] ) ;
 			}
 		}
